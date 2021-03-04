ee2992249c3b09ba88065d2e4d228452d891798e
Vanished <ibrahimcajlakovic1@gmail.com>
2021-03-04 20:41:39 +0100

    Update Codes.js

--- src/js/Codes.js
+++ src/js/Codes.js
@@ -7,14 +7,15 @@ var codesArray = [];
 
 var Codes = {
     fetch: (id = null) => {
+        console.log('fetch');
         axios.get('https://144.172.75.30:8000/codes').then((response) => {
             // ...
-            console.log(response);
             
             return response.data.codes;
         }).catch(console.error);
     },
     insert: () => {
+        console.log('insert');
         var codes = Util.prototype.getFolderColors();
 
         if (codes.length === 0) return;
