1fe430912bb47b6a59f254b081777cdaa816fdac
vanishedvan <vanished.apps@gmail.com>
2021-03-07 12:18:40 +0100

    Added and changed the tooltip text.

--- src/js/Code.js
+++ src/js/Code.js
@@ -21,7 +21,7 @@ class Code extends React.Component {
         
         return (
             <CopyToClipboard text={codeString || null} key="0">
-                <div className="button-content-double-item" tag="clickable-item" data-tip="hello world">
+                <div className="button-content-double-item" tag="clickable-item" data-tip="Gradient Code">
                     <div className="button-content-item-icon">
                         <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" viewBox="0 0 24 24" >
                             <path d="M9.95263 16.9123L8.59323 18.3608L2.03082 12.2016L8.18994 5.63922L9.64826 7.00791L4.85783 12.112L9.95212 16.8932L9.95263 16.9123Z" />
@@ -31,13 +31,7 @@ class Code extends React.Component {
                     <p className="button-content-item-text">
                         {codeString || ''}
                     </p>
-                    <ReactTooltip place="top" effect="solid" wrapper="div" className="main-tooltip" backgroundColor="#4f5660">
-                        <p>
-                            Use d!roles [code] to export your generated gradient as roles in your Discord server!
-                            <br />
-                            Click <a href="https://google.com">here</a> to add the Discord bot to your server.
-                        </p>
-                    </ReactTooltip>
+                    <ReactTooltip place="top" effect="solid" className="code-tooltip" backgroundColor="#4f5660" />
                 </div>
             </CopyToClipboard>
         );
