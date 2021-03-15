83ee47f1c6ad7669ff719dd60bd1de877e3e5101
vanishedvan <vanished.apps@gmail.com>
2021-03-15 21:21:59 +0100

    :art: Restructured the color manager system.

--- src/utility/ColorsManager.js
+++ src/utility/ColorsManager.js
@@ -12,20 +12,6 @@ class ColorsManager extends React.Component {
     
         this.state = {
             colorArray: [
-                (
-                    <div className="colors-wrapper-item" key="0">
-                        <div className="colors-wrapper-item-color"></div>
-                        <input className="colors-wrapper-item-input" onChange={ () => { UpdateColorBox(); GradientManager.prototype.generateGradient(); } } placeholder="#000000" maxLength="7" />
-                    </div>
-                ),
-                (
-                    <div className="colors-wrapper-item" key="1">
-                        <div className="colors-wrapper-item-color"></div>
-                        <input className="colors-wrapper-item-input" onChange={ () => { UpdateColorBox(); GradientManager.prototype.generateGradient(); } } placeholder="#000000" maxLength="7" />
-                    </div>
-                )
-            ],
-            rawColorArray: [
                 '#000000',
                 '#000000'
             ]
@@ -35,33 +21,24 @@ class ColorsManager extends React.Component {
         this.setState = this.setState.bind(this);
     }
 
-    getColors(rawColors = false) {
+    getColors() {
         // ...
 
-        if (rawColors) return currentComponent.state.rawColorArray;
-        else return currentComponent.state.colorArray;
+        return currentComponent.state.colorArray;
     }
     addColor(hexColor) {
         // ...
 
-        var colorArray = currentComponent.state.colorArray,
-            rawColorArray = currentComponent.state.rawColorArray;
+        var colorArray = currentComponent.state.colorArray;
 
         if (currentComponent.state.colorArray.length === 25) return currentComponent.state.colorArray;
 
-        colorArray.push(
-            <div className="colors-wrapper-item" key={ colorArray.length }>
-                <div className="colors-wrapper-item-color" style={{ backgroundColor: hexColor }}></div>
-                <input className="colors-wrapper-item-input" onChange={ () => { UpdateColorBox(); GradientManager.prototype.generateGradient(); } } placeholder="#000000" maxLength="7" />
-            </div>
-        );
-        rawColorArray.push(hexColor);
+        colorArray.push(hexColor);
 
         if (PointsManager.prototype.getPointAmount() < currentComponent.state.colorArray.length) PointsManager.prototype.setPointAmount(currentComponent.state.colorArray.length);
 
         currentComponent.setState(() => ({
-            colorArray: colorArray,
-            rawColorArray: rawColorArray
+            colorArray: colorArray
         }));
 
         return currentComponent.state.colorArray;
@@ -69,45 +46,43 @@ class ColorsManager extends React.Component {
     removeColor() {
         // ...
 
-        var colorArray = currentComponent.state.colorArray,
-            rawColorArray = currentComponent.state.rawColorArray;
+        var colorArray = currentComponent.state.colorArray;
 
         if (colorArray.length === 2) return currentComponent.state.colorArray;
         
         colorArray.pop();
-        rawColorArray.pop();
 
         currentComponent.setState(() => ({
-            colorArray: colorArray,
-            rawColorArray: rawColorArray
+            colorArray: colorArray
         }));
 
         return currentComponent.state.colorArray;
     }
-    updateColors(newColorArray, rawColors = false) {
+    updateColors(newColorArray) {
         // ...
 
-        if (rawColors) {
-            currentComponent.setState((stateObject) => ({
-                colorArray: stateObject.colorArray,
-                rawColorArray: newColorArray
-            }));
+        currentComponent.setState((stateObject) => ({
+            colorArray: newColorArray
+        }));
 
-            return currentComponent.state.rawColorArray;
-        } else {
-            currentComponent.setState((stateObject) => ({
-                colorArray: newColorArray,
-                rawColorArray: stateObject.rawColorArray
-            }));
-
-            return currentComponent.state.colorArray;
-        }
+        return currentComponent.state.colorArray;
     }
 
     render() {
         // ...
 
-        return currentComponent.state.colorArray;
+        var colorArray = [];
+
+        currentComponent.state.colorArray.forEach((hexColor) => {
+            colorArray.push(
+                <div className="colors-wrapper-item" key={ colorArray.length }>
+                    <div className="colors-wrapper-item-color" style={{ backgroundColor: hexColor }}></div>
+                    <input className="colors-wrapper-item-input" onChange={ () => { UpdateColorBox(); GradientManager.prototype.generateGradient(); } } placeholder="#000000" maxLength="7" />
+                </div>
+            );
+        });
+
+        return colorArray;
     }
 }
 export default ColorsManager;
