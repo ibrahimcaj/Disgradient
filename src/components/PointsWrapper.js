55e58ec9f0c4b806aec60ed9ec049204208572a3
vanishedvan <vanished.apps@gmail.com>
2021-04-05 21:21:02 +0200

    :bug: Fixed bug where add icon was transitioning.

--- src/components/PointsWrapper.js
+++ src/components/PointsWrapper.js
@@ -18,7 +18,7 @@ class PointsWrapper extends React.Component {
                 <div className="points-wrapper-button-wrapper">
                     <div className="points-wrapper-button" onClick={ () => PointsManager.prototype.addPointAmount() }>
                         <div className="points-wrapper-button-icon">
-                            <svg xmlns="http://www.w3.org/2000/svg" className="points-wrapper-button-icon" viewBox="0 0 16 16">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16">
                                 <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
                             </svg>
                         </div>
