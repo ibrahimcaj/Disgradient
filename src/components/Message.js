48400d1bcd46abc7b4a764c8dcacf2f549d3f8ea
Vanished <vanished.apps@gmail.com>
2021-10-28 17:23:26 +0200

    fix: Issues with Helmet and <link>, also uninstalled react-helmet.

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import Helmet from 'react-helmet';
 
 import Wumpus from '../icons/wumpus.png';
 
@@ -9,24 +8,18 @@ import '../stylesheets/Messages.css';
 class Message extends React.Component {
     render() {
         return (
-            <>
-                <Helmet>
-                    <link href="http://fonts.cdnfonts.com/css/whitney-2" rel="stylesheet" />
-                </Helmet>
-
-                <div className="message-item" key={this.props.index}>
-                    <div className="message-avatar" style={{ backgroundImage: `url(${Wumpus})` }}></div>
+            <div className="message-item" key={this.props.index}>
+                <div className="message-avatar" style={{ backgroundImage: `url(${Wumpus})` }}></div>
+                <div>
                     <div>
-                        <div>
-                            <span className="message-author" style={{ color: this.props.color }}>Wumpus</span>
-                            <span className="message-date">Today at 00:00</span>
-                        </div>
-                        <p className="message-content">
-                            rocks are really old
-                        </p>
+                        <span className="message-author" style={{ color: this.props.color }}>Wumpus</span>
+                        <span className="message-date">Today at 00:00</span>
                     </div>
+                    <p className="message-content">
+                        rocks are really old
+                    </p>
                 </div>
-            </>
+            </div>
         );
     }
 }
